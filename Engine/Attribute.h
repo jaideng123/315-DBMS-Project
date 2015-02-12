3265d5761f17760189beba14065880fda2842101
jaideng123 <jaideng123@yahoo.com>
2015-02-12 14:41:00 -0600

    added print function for table

--- Engine/Attribute.h
+++ Engine/Attribute.h
@@ -15,6 +15,7 @@ public:
 
 	//needs set and get functions
 	string get_name(){return name;}
+	string get_type(){return type;}
 	Attribute& operator=(const Attribute& rhs){
 	name = rhs.name;
 	return *this;
