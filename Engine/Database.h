baa9b6f1cccef126fe66715093f56f97d456250f
Jaiden Gerig <jaideng123@yahoo.com>
2015-02-12 16:42:31 -0600

    Added more detail to query functions

--- Engine/Database.h
+++ Engine/Database.h
@@ -24,11 +24,11 @@ public:
 
 	//Query functions
 	Table set_select(Table t1,vector<int> record_IDs){}
-	Table set_union(Table tab1, Table tab2){}
-	Table set_difference(Table t1,Table t2){}
-	Table set_product(Table t1,Table t2){}
-	Table set_project(Table t1, vector<string> attribute_names){}
-	Table set_rename(Table t1, vector<string> attribute_names){}
+	Table set_union(Table t1, Table t2){}//everything in t1  + everything in t2 not in t1
+	Table set_difference(Table t1,Table t2){}//everything in t1 that is not in t2
+	Table set_product(Table t1,Table t2){} //combine Attributes and Records
+	Table set_project(Table t1, vector<string> attribute_names){}//select only certain attributes
+	Table set_rename(Table t1, vector<string> attribute_names){}//rename all attributes
 
 };
 #endif
