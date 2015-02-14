9a0ed3f7374bb47bc40a5d0f630152ef4aefed7d
tle17 <tle17@tamu.edu>
2015-02-13 19:28:18 -0600

    did set_union

--- Engine/Database.cpp
+++ Engine/Database.cpp
@@ -173,18 +173,22 @@ void Database::close(string table_name) {
 
 
 //Query functions
-//NOT DONE
-// Table Database::set_union(Table t1, Table t2){
-// 	Table union("set_union");
-// 	//adding attributes
-// 	int t1_attSize = t1.att_size();
-// 	int t2_attSize = t2.att_size();
-// 	for(int i = 0; i < t1_attSize; i++){
-// 		for(int j = 0; j < t2_attSize; j++){
-// 			if(t1.get)	
-// 		}
-// 	}
-// }
+Table Database::set_union(Table t1, Table t2){
+	if(union_compatible(t1, t2))
+	{
+		Table sunion(t1.get_name());
+		//adding attributes
+		for (int i = 0; i < t1.get_records().size(); ++i)
+		{
+			if(record_exists(t2,t1.get_records()[i]))
+				sunion.add_record(t1.get_records()[i]);
+		}
+		return sunion;
+	}
+	else
+		cout<<"Sets are incompatible"<<endl;
+	return NULL;
+}
 
 //
 Table Database::set_difference(Table t1, Table t2){
