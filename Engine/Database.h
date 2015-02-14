207c3460a76f64b7d20342b85973bec92980fb01
jaideng123 <jaideng123@yahoo.com>
2015-02-13 21:14:25 -0600

    cleaned up Database.h

--- Engine/Database.h
+++ Engine/Database.h
@@ -18,33 +18,46 @@ public:
 	vector<Table> tables;
 
 	//Command functions
+
+	//bring table into memory from disk
 	void open(string table_name);
+	//remove table from memory
 	void close(string table_name); 
+	//write table to disk
 	void write(string table_name);
+	//create a new table and insert it into tables
 	void create(string table_name, vector<Attribute> v);
+	//insert a record into a table in tables
 	void insert(string table_name, Record record);
+	//prints table
 	void show(string table_name);
+	//swaps out records at the indices in old records for new ones
 	void update(string table_name,vector<Record> new_records,vector<int> old_records){}
+	//delete records based on index
 	void delete_records(string table_name,vector<int> to_remove);
 
 	//Query functions
+
+	//select based on record ID's(position in table)
 	Table set_select(Table t1,vector<int> record_IDs){}
-	Table set_union(Table t1, Table t2);//everything in t1  + everything in t2 not in t1
-	Table set_difference(Table t1,Table t2);//everything in t1 that is not in t2
-	Table set_product(Table t1,Table t2); //combine Attributes and Records
-	Table set_project(Table t1, vector<string> attribute_names);//select only certain attributes
-	Table set_rename(Table t1, vector<string> attribute_names){}//rename all attributes
-	
+	//everything in t1  + everything in t2 not in t1
+	Table set_union(Table t1, Table t2);
+	//everything in t1 that is not in t2
+	Table set_difference(Table t1,Table t2);
+	//combine Attributes and Records
+	Table set_product(Table t1,Table t2);
+	//select only certain attributes
+	Table set_project(Table t1, vector<string> attribute_names);
+	//rename all attributes
+	Table set_rename(Table t1, vector<string> attribute_names){}
+
+
+private:
 	//Helper Functions
 	void get_attributes_from_file(ifstream &input,vector<Attribute> &attributes); 
 	void get_records_from_file(ifstream &input ,
 		vector<Record> &records, int num_attributes);
-
-	//need function to test union compatability
-	//union compatible = same exact set of attributes
-
-private:
-	//make sure you call this before you call find_table
+	
 	bool union_compatible(Table t1, Table t2);
 	bool table_exists(string table_name);
 	bool record_exists(Table t1,Record rec);
