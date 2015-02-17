1bb74921f713b5a6f936f35b3d678c8967f71b0e
jaideng123 <jaideng123@yahoo.com>
2015-02-17 15:29:49 -0600

    Scaffolding for parser

---
+++ Parser/Parser.cpp
@@ -0,0 +1,27 @@
+#include "Parser.h"
+
+using namespace std;
+
+void Parser::test_parse(vector<Token> input_tokens){
+	tokens = input_tokens;
+	current_token = 0;
+	if(tokens[current_token].get_type() == Token::IDENTIFIER){
+		query();
+	}
+	else{
+		command();
+	}
+
+}
+
+void Parser::condition(){
+
+}
+
+void Parser::query(){
+
+}
+
+void Parser::command(){
+
+}
--- Parser/Parser.h
+++ Parser/Parser.h
@@ -1,7 +1,24 @@
 #ifndef _parser_h_                   // include file only once
 #define _parser_h_
 
-//Takes a vector of tokens, then does it's thing
 #include "Tokenizer.h"
+#include <vector>
+
+class Parser{
+public:
+	Parser(){}
+
+	void parse(string input){}//use test_parse till tokenizer is done
+	void test_parse(vector<Token> tokens);
+
+private:
+	vector<Token> tokens;
+	int current_token;
+
+	void command();
+	void query();
+	void condition();
+
+};
 
 #endif
