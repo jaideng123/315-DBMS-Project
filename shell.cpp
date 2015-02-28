7171f4929c9c47cf977275c344a41137073ac009
Jaiden Lee Gerig <jaideng@linux2.cs.tamu.edu>
2015-02-27 19:12:14 -0600

    Cleaned up the repo

--- shell.cpp
+++ shell.cpp
@@ -5,30 +5,6 @@ using namespace std;
 
 int main(){
 	DBMS db;
-	vector<Attribute> attr;
-
- 	attr.push_back(Attribute("id","INTEGER"));
-	attr.push_back(Attribute("name","VARCHAR(20)"));
-	attr.push_back(Attribute("description","STRING"));
-	attr.push_back(Attribute("user_id","INTEGER"));
-
-	Table test("test",attr);
-
-	for (int i = 0; i < 20; ++i){
-		Record r;
-
-		//c++ 11 features
-		string pid = to_string(i);
-		string uid = to_string(i*20);
-
-		r.add_entry(pid);
-		r.add_entry("Dummy String");
-		r.add_entry("Dummy Text");
-		r.add_entry(uid);
-
-		test.add_record(r);
-	}
-	db.db.tables.push_back(test);
 	string command;
 	while(true){
 		cout<<">";
